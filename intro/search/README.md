3fc3161193e904dacbc1ededaab861bfc69084f0
Kiernan Beresford <beresford.kiernan@gmail.com>
2026-02-10 22:00:09 +0000

    add video and comment

--- intro/search/README.md
+++ intro/search/README.md
@@ -54,3 +54,7 @@
    1. Finally, compare and contrast the randomized search algorithm with linear search and binary search in terms of time complexity, data requirements (such as ordering), and practical efficiency. Discuss scenarios in which each approach may be preferred, highlighting the advantages and limitations of randomized search relative to linear and binary search.
       - Random and Linear both have the same time complexity. Despite the fact that in the random search implementation we iterate over the length of the array twice. Because it would be 2n we drop the constant and consider it O(N) time complexity.
       - Linear search iterates over early elements more frequently than elements at the end of the array. If that isn't desired behavior random search offers an alternative.
+
+   ## Video
+
+   [Canvas Video](https://sdccd.us-west-2.instructuremedia.com/embed/3b84de61-ddb5-4eb7-8387-0639ad7784d5)
--- intro/search/main.cpp
+++ intro/search/main.cpp
@@ -10,29 +10,29 @@ using namespace std;
 
 int main()
 {
-    vector<int> sorted_vec{2, 4, 6, 8, 10, 12, 13};
-    vector<int> big_vec(100000);
-    // populate big_vec with indexes
-    for (int i = 0; i < (int)big_vec.size(); ++i)
-        big_vec[i] = i;
+     vector<int> sorted_vec{2, 4, 6, 8, 10, 12, 13};
+     vector<int> big_vec(100000);
+     // populate big_vec with indexes
+     for (int i = 0; i < (int)big_vec.size(); ++i)
+          big_vec[i] = i;
+     // init vars
+     int steps;
+     int bin_search_worst = static_cast<int>(ceil(log2(100000)));
+     int lin_search_worst = static_cast<int>(big_vec.size());
 
-    int steps;
-    int bin_search_worst = static_cast<int>(ceil(log2(100000)));
-    int lin_search_worst = static_cast<int>(big_vec.size());
+     cout << "Worst case number of steps linear search: " << lin_search_worst << "\n";
+     cout << "Worst case number of steps binary search: " << bin_search_worst << "\n";
 
-    cout << "Worst case number of steps linear search: " << lin_search_worst << "\n";
-    cout << "Worst case number of steps binary search: " << bin_search_worst << "\n";
+     cout << "Linear search idx: " << linearSearch(big_vec.data(), big_vec.size(), -1, steps)
+          << " steps: " << steps << "\n";
+     // this shows true worst case
+     cout << "Binary search idx: " << binarySearch(big_vec.data(), big_vec.size(), 100000, steps)
+          << " steps: " << steps << "\n";
 
-    cout << "Linear search idx: " << linearSearch(big_vec.data(), big_vec.size(), -1, steps)
-         << " steps: " << steps << "\n";
-    // this shows true worst case
-    cout << "Binary search idx: " << binarySearch(big_vec.data(), big_vec.size(), 100000, steps)
-         << " steps: " << steps << "\n";
+     cout << "Random search idx: " << randomSearch(big_vec.data(), big_vec.size(), -1, steps)
+          << " steps: " << steps << "\n";
+     // flush
+     cout << endl;
 
-    cout << "Random search idx: " << randomSearch(big_vec.data(), big_vec.size(), -1, steps)
-         << " steps: " << steps << "\n";
-    // flush
-    cout << endl;
-
-    return 0;
+     return 0;
 }
