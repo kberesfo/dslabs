a53d886f1619875c902787d180b38897120f3d72
Kiernan Beresford <beresford.kiernan@gmail.com>
2026-03-28 04:22:47 +0000

    update to prevent heap allocation

--- intro/binary-search-trees-activity/src/main.cpp
+++ intro/binary-search-trees-activity/src/main.cpp
@@ -8,18 +8,18 @@ using namespace std;
 int main()
 {
     vector<int> inputVec{1, 5, 9, 2, 4, 10, 6, 3, 8};
-    Tree *tree = new Tree();
+    Tree tree = Tree();
     int steps = 0;
 
     for (int i = 0; i < inputVec.size(); ++i)
     {
-        tree->insert(inputVec[i]);
+        tree.insert(inputVec[i]);
     }
 
-    tree->find(10, steps);
-    cout << "It took: " << steps << " to find 10\n\n";
-    tree->find(40, steps);
-    cout << "It took: " << steps << " to search for 40\n\n";
+    tree.find(10, steps);
+    cout << "It took: " << steps << " steps to find 10\n";
+    tree.find(40, steps);
+    cout << "It took: " << steps << " steps to search for 40\n\n";
 
     return 0;
 }
